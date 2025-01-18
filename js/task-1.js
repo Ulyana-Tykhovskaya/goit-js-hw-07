2c2e37dc024e8a6a32ac494f7db005dded831a9e
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-01-18 12:32:40 +0100

    Update

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const ulElement = document.querySelector("#categories");
+const liElement = ulElement.querySelectorAll(".item");
+console.log(`${liElement.length}`);
+liElement.forEach((items) => {
+  const title = items.querySelector("h2").textContent;
+  const elementCount = items.querySelectorAll("ul > li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementCount}`);
+});
